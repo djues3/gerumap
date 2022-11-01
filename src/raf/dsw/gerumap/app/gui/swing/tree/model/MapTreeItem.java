c6728a71a056ec121eb8b6393034f9c688bed99a
themadseventeen <dobradovic13122rn@raf.rs>
2022-11-01 16:56:31 +0100

    dodat JTree sa leve strane

---
+++ src/raf/dsw/gerumap/app/gui/swing/tree/model/MapTreeItem.java
@@ -0,0 +1,27 @@
+package raf.dsw.gerumap.app.gui.swing.tree.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import raf.dsw.gerumap.app.mapRepository.MapNode;
+
+import javax.swing.tree.DefaultMutableTreeNode;
+
+@Getter
+@Setter
+public class MapTreeItem extends DefaultMutableTreeNode {
+
+    private MapNode mapNode;
+
+    public MapTreeItem(MapNode nodeModel) {
+        this.mapNode = nodeModel;
+    }
+
+    @Override
+    public String toString() {
+        return mapNode.getName();
+    }
+
+    public void setName(String name) {
+        this.mapNode.setName(name);
+    }
+}
--- src/raf/dsw/gerumap/app/mapRepository/model/Project.java
+++ src/raf/dsw/gerumap/app/mapRepository/model/Project.java
@@ -1,6 +1,8 @@
 package raf.dsw.gerumap.app.mapRepository.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import raf.dsw.gerumap.app.mapRepository.MapNode;
 import raf.dsw.gerumap.app.mapRepository.MapNodeComposite;
@@ -9,6 +11,8 @@ import java.nio.file.Path;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Project extends MapNodeComposite {
 
 	private Path filePath;
