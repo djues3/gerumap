be309e9f7221d229be501f192be4b9145c9fcbd6
djues3 <djues3@gmail.com>
2022-11-15 16:21:00 +0100

    Dodat error za: ProjectExplorer cannot be removed

--- src/main/raf/dsw/gerumap/app/mapRepository/factory/FactoryUtil.java
+++ src/main/raf/dsw/gerumap/app/mapRepository/factory/FactoryUtil.java
@@ -5,11 +5,14 @@ import raf.dsw.gerumap.app.mapRepository.factory.factories.MindMapFactory;
 import raf.dsw.gerumap.app.mapRepository.factory.factories.ProjectFactory;
 
 public class FactoryUtil {
+	private static final ProjectFactory projectFactory = new ProjectFactory();
+	private static final MindMapFactory mindMapFactory = new MindMapFactory();
+	private static final ElementFactory elementFactory = new ElementFactory();
 	public static NodeFactory getFactory(String parentType) {
 		return switch (parentType) {
-			case "ProjectExplorer" -> new ProjectFactory();
-			case "Project" -> new MindMapFactory();
-			case "MindMap" -> new ElementFactory();
+			case "ProjectExplorer" -> projectFactory;
+			case "Project" -> mindMapFactory;
+			case "MindMap" -> elementFactory;
 			default -> null;
 		};
 	}
