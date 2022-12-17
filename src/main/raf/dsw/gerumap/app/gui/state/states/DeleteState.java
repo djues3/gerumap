749d4bc2f8103fd10a35dbeb45cb7bd6c7110512
themadseventeen <dobradovic13122rn@raf.rs>
2022-12-17 23:01:31 +0100

    haha fix

--- src/main/raf/dsw/gerumap/app/gui/state/states/DeleteState.java
+++ src/main/raf/dsw/gerumap/app/gui/state/states/DeleteState.java
@@ -7,13 +7,17 @@ import raf.dsw.gerumap.app.gui.swing.view.ProjectView;
 import raf.dsw.gerumap.app.gui.swing.view.painter.LinkPainter;
 import raf.dsw.gerumap.app.gui.swing.view.painter.Painter;
 import raf.dsw.gerumap.app.gui.swing.view.painter.TermPainter;
+import raf.dsw.gerumap.app.mapRepository.MapNode;
+import raf.dsw.gerumap.app.mapRepository.MapNodeComposite;
 import raf.dsw.gerumap.app.mapRepository.model.MindMap;
 import raf.dsw.gerumap.app.mapRepository.model.elements.Link;
 import raf.dsw.gerumap.app.mapRepository.model.elements.Term;
 
 import java.awt.geom.NoninvertibleTransformException;
 import java.awt.geom.Point2D;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 public class DeleteState extends State {
 	@Override
@@ -28,19 +32,19 @@ public class DeleteState extends State {
 		x = (int)real.getX();
 		y = (int)real.getY();
 		MindMap map = view.getMindMap();
+		Term termToRemove = null;
+		List<Link> linksToRemove = new ArrayList<>();
 		for(Painter p: view.getPainters()) {
 			if(p instanceof TermPainter tp) {
 				if(tp.elementAt(x, y)) {
 					Term term = tp.getTerm();
 					view.removePainter(tp);
-					Iterator<Link> it = term.getLinks().iterator();
-					while(it.hasNext()) {
-						Link l = it.next();
-						view.removePainter(view.getPainterForLink(l));
-						l.getFrom().getLinks().remove(l);
-						l.getTo().getLinks().remove(l);
-						it.remove();
-					}
+					termToRemove = term;
+					//						view.removePainter(view.getPainterForLink(l));
+					//						l.getFrom().getLinks().remove(l);
+					//						l.getTo().getLinks().remove(l);
+					//						it.remove();
+					linksToRemove.addAll(term.getLinks());
 					break;
 				}
 			}
@@ -48,11 +52,17 @@ public class DeleteState extends State {
 				if(lp.elementAt(x, y)) {
 					lp.getLink().getFrom().getLinks().remove(lp.getLink());
 					lp.getLink().getTo().getLinks().remove(lp.getLink());
-					map.removeChild(lp.getLink());
+//					map.removeChild(lp.getLink());
 					view.removePainter(lp);
 					break;
 				}
 			}
+		}
+		if (termToRemove != null)
+			view.removePainter(view.getPainterForTerm(termToRemove));
+		for (Link l : linksToRemove) {
+			map.removeChild(l);
+			view.removePainter(view.getPainterForLink(l));
 		}
 		view.repaint();
 	}
--- src/main/raf/dsw/gerumap/app/mapRepository/model/elements/Link.java
+++ src/main/raf/dsw/gerumap/app/mapRepository/model/elements/Link.java
@@ -7,6 +7,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import raf.dsw.gerumap.app.mapRepository.model.Element;
 
+import java.util.Objects;
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -14,4 +16,14 @@ import raf.dsw.gerumap.app.mapRepository.model.Element;
 public class Link extends Element {
 	private Term from;
 	private Term to;
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Link link = (Link) o;
+		return (Objects.equals(from, link.from) && Objects.equals(to, link.to)) ||
+				(Objects.equals(from, link.to) && Objects.equals(to, link.from));
+	}
+
 }
