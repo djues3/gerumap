47dd15361525275da4326209f7d252199cc34674
themadseventeen <dobradovic13122rn@raf.rs>
2023-01-03 16:43:47 +0100

    Implementiran undo/redo za pomeranje pojmova

--- src/main/raf/dsw/gerumap/app/gui/state/states/MoveState.java
+++ src/main/raf/dsw/gerumap/app/gui/state/states/MoveState.java
@@ -4,15 +4,19 @@ import lombok.Getter;
 import lombok.Setter;
 import raf.dsw.gerumap.app.AppCore;
 import raf.dsw.gerumap.app.gui.state.State;
+import raf.dsw.gerumap.app.gui.swing.commands.implementation.AddTermCommand;
+import raf.dsw.gerumap.app.gui.swing.commands.implementation.MoveCommand;
 import raf.dsw.gerumap.app.gui.swing.view.MainFrame;
 import raf.dsw.gerumap.app.gui.swing.view.MindMapView;
 
 import raf.dsw.gerumap.app.gui.swing.view.ProjectView;
 import raf.dsw.gerumap.app.gui.swing.view.painter.TermPainter;
+import raf.dsw.gerumap.app.mapRepository.model.Element;
 import raf.dsw.gerumap.app.mapRepository.model.elements.Term;
 
 import java.awt.geom.NoninvertibleTransformException;
 import java.awt.geom.Point2D;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,6 +25,7 @@ import java.util.List;
 public class MoveState extends State {
 	private int startX;
 	private int startY;
+	private int startXReal, startYReal, endXReal, endYReal;
 	List<TermPainter> toMove;
 
 	private boolean viewMove = false;
@@ -35,6 +40,8 @@ public class MoveState extends State {
 		}
 		x = (int)real.getX();
 		y = (int)real.getY();
+		startXReal = x;
+		startYReal = y;
 		Term term = view.getMindMap().getTermAt(x, y);
 		toMove = new ArrayList<>();
 		if(term == null)
@@ -78,5 +85,28 @@ public class MoveState extends State {
 		view.repaint();
 		startX = (int)screen.getX();
 		startY = (int)screen.getY();
+	}
+
+	@Override
+	public void mouseReleased(int x, int y, MindMapView view) {
+		Point2D real = new Point2D.Double();
+		Point2D screen = new Point2D.Double(x, y);
+		try {
+			((ProjectView)MainFrame.getInstance().getProjectView()).getMindMapView().getAffineTransform().inverseTransform(screen, real);
+		} catch (NoninvertibleTransformException e) {
+			AppCore.getInstance().getLogger().log(e);
+		}
+		endXReal = (int)real.getX();
+		endYReal = (int)real.getY();
+
+		ArrayList<Element> elements = new ArrayList<>();
+		for (TermPainter tp : toMove) {
+			elements.add((Element) tp.getTerm());
+			tp.getTerm().setX(tp.getTerm().getX() - (endXReal - startXReal));
+			tp.getTerm().setY(tp.getTerm().getY() - (endYReal - startYReal));
+		}
+		MoveCommand command = new MoveCommand(elements, endXReal - startXReal, endYReal - startYReal, view);
+		AppCore.getInstance().getMapRepository().getCommandManager().addCommand(command);
+		view.repaint();
 	}
 }
